96a426a4efadec767c281eb7644bfb7b72694dc0
Dmytro Tristan <dmytro.tristan@dxc.com>
2023-01-14 23:05:35 +0100

    Implementation added and tested

---
+++ inc/inet_socket.hpp
@@ -0,0 +1,79 @@
+#ifndef INET_SOCKET_HPP
+#define INET_SOCKET_HPP
+
+#include "socket_common.hpp"
+#include "ssl.hpp"
+
+namespace tristan::sockets {
+
+    class InetSocket {
+    public:
+        explicit InetSocket(SocketType socket_type = SocketType::STREAM);
+        InetSocket(const InetSocket& other) = delete;
+        InetSocket(InetSocket&& other) = delete;
+
+        InetSocket& operator=(const InetSocket& other) = delete;
+        InetSocket& operator=(InetSocket&& other) = delete;
+
+        ~InetSocket();
+
+        void setHost(uint32_t ip, const std::string& host_name = "");
+        void setPort(uint16_t port);
+        void setNonBlocking(bool non_blocking = true);
+        void resetError();
+        void bind();
+        void listen(uint32_t connection_count_limit);
+        void connect(bool ssl = true);
+        void close();
+        void shutdown();
+        [[nodiscard]] auto accept() -> std::optional<std::unique_ptr<InetSocket>>;
+
+        [[nodiscard]] auto write(const std::vector< uint8_t >& data, uint16_t size = 0, uint64_t offset = 0) -> uint64_t;
+
+        template < class ObjectClassToSend >
+        [[nodiscard]] auto write(ObjectClassToSend object) -> uint64_t
+            requires std::is_standard_layout_v< ObjectClassToSend >
+        {
+            std::vector< uint8_t > temp_data(reinterpret_cast< uint8_t* >(object), reinterpret_cast< uint8_t* >(object) + sizeof(object));
+            return InetSocket::write(temp_data);
+        }
+
+        [[nodiscard]] auto read() -> uint8_t;
+        [[nodiscard]] auto read(uint16_t size) -> std::vector< uint8_t >;
+        [[nodiscard]] auto readUntil(uint8_t delimiter) -> std::vector< uint8_t >;
+        [[nodiscard]] auto readUntil(const std::vector< uint8_t >& delimiter) -> std::vector< uint8_t >;
+
+        [[nodiscard]] auto ip() const noexcept -> uint32_t;
+        [[nodiscard]] auto port() const noexcept -> uint16_t;
+        [[nodiscard]] auto error() const noexcept -> std::error_code;
+        [[nodiscard]] auto nonBlocking() const noexcept -> bool;
+        [[nodiscard]] auto connected() const noexcept -> bool;
+
+    protected:
+    private:
+
+        explicit InetSocket(bool);
+
+        std::string m_host_name;
+
+        int32_t m_socket;
+        uint32_t m_ip;
+
+        std::error_code m_error;
+
+        uint16_t m_port;
+
+        std::unique_ptr<Ssl> m_ssl;
+        SocketType m_type;
+
+
+        bool m_non_blocking;
+        bool m_bound;
+        bool m_listening;
+        bool m_not_ssl_connected;
+        bool m_connected;
+    };
+
+}  // namespace tristan::sockets
+
+#endif  //INET_SOCKET_HPP
---
+++ inc/ssl.hpp
@@ -0,0 +1,65 @@
+#ifndef OPEN_SSL_HPP
+#define OPEN_SSL_HPP
+
+#include <openssl/crypto.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace tristan::sockets {
+
+    class Ssl {
+        friend class InetSocket;
+
+    public:
+        Ssl(const Ssl& other) = delete;
+        Ssl(Ssl&& other) = delete;
+
+        Ssl& operator=(const Ssl& other) = delete;
+        Ssl& operator=(Ssl&& other) = delete;
+
+        static auto create(int32_t socket) -> std::unique_ptr< Ssl >;
+
+        ~Ssl();
+
+    private:
+        explicit Ssl(int32_t socket);
+
+        auto connect() -> std::error_code;
+
+        auto verifyHost(const std::string& host) -> bool;
+        auto verifyIp(uint32_t ip) -> bool;
+        auto verifyIp(uint64_t ip) -> bool;
+        auto verifyStartDate() -> bool;
+        auto verifyEndDate() -> bool;
+
+        auto write(const std::vector< uint8_t >& data, uint16_t size = 0, uint64_t offset = 0) -> std::pair< std::error_code, uint64_t >;
+
+        template < class ObjectClassToSend >
+        auto write(ObjectClassToSend object) -> std::pair< std::error_code, uint64_t >
+            requires std::is_standard_layout_v< ObjectClassToSend >
+        {
+            std::vector< uint8_t > temp_data(reinterpret_cast< uint8_t* >(object), reinterpret_cast< uint8_t* >(object) + sizeof(object));
+            return Ssl::write(temp_data);
+        }
+
+        [[nodiscard]] auto read() -> std::pair< std::error_code, uint8_t >;
+        [[nodiscard]] auto read(std::vector<uint8_t>& data, uint16_t size) -> std::pair< std::error_code, std::vector< uint8_t > >;
+
+        void shutdown();
+
+        SSL_CTX* m_context;
+        SSL* m_ssl;
+        X509* m_server_certificate;
+
+        const SSL_METHOD* method;
+    };
+
+}  // namespace tristan::sockets
+
+#endif  //OPEN_SSL_HPP
